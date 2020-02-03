ea1ad1383a1e06f6c72fa7b683408eb896cc04b2
Julio César Godínez <jcgodgar@icloud.com>
2020-02-03 14:55:11 -0600

    Initial Commit

---
+++ PerfectPassword/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PerfectPassword
+//
+//  Created by Ago Moviles on 2/3/20.
+//  Copyright © 2020 Julio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
